9ccefb10b2db325ed429863f330abddb2b8b84ad
SVishC <chaitanyamvishnu@gmail.com>
2016-03-16 17:09:04 +0530

    Solution created

---
+++ Mth Number from a list/Mth Number from a list/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mth_Number_from_a_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the numbers");
+            int index = 0;
+            long result = -1;
+            Int32.TryParse(Console.ReadLine(), out index);
+            bool go = true;
+            string str_num = Console.ReadLine();
+            object[] numbers = str_num.Split(' ');
+
+            if (numbers.Length > 0 && numbers.Length > index && index > 0)
+            {
+                result = Convert.ToInt64(numbers[numbers.Length - index]);
+            }
+            if (index <= 0 || (index - numbers.Length) > 0)
+            {
+                go = false;
+            }
+            Console.WriteLine(go ? result.ToString() : "NIL");
+
+            //Console.WriteLine(str);
+        }
+    }
+}
