c0fea9f1fadef7cbe376893183fd923b2097047f
SVishC <chaitanyamvishnu@gmail.com>
2016-03-16 17:25:12 +0530

    Nth Fibonacci Program coded

---
+++ nthFibonacciSeriesValue-HackerRank/nthFibonacciSeriesValue-HackerRank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nthFibonacciSeriesValue_HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long inp;
+            Int64.TryParse(Console.ReadLine(), out inp);
+            if(inp>=0)
+            Console.WriteLine(Fibonacci(inp));
+
+        }
+        public static int Fibonacci(long n) 
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
